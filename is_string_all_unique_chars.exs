f8c74b1e48d4d5de522cb1f656c16a9dccf7c016
Gustavo Saiani <gustavo@poe.ma>
2017-04-22 18:28:19 -0300

    Is String made up of all unique chars?

---
+++ is_string_all_unique_chars.exs
@@ -0,0 +1,41 @@
+defmodule IsStringUnique do
+  @moduledoc """
+  Chapter 1
+  Page 90
+
+  1.1 Is Unique:
+  Implement an algorithm to determine if a string has all unique characters.
+  What if you cannot use additional data structures?
+  """
+
+  def start(str) when is_binary(str) and str == "", do: IO.puts("String is empty.")
+  def start(str) when is_binary str do
+    has_duplicate? =
+      str
+      |> String.codepoints
+      |> found_duplicate?
+
+    case has_duplicate? do
+      false -> IO.inspect("String #{str} has all unique characters.")
+      _     -> IO.inspect("String #{str} has duplicated characters")
+    end
+  end
+
+  def found_duplicate?([]), do: false
+  def found_duplicate?([char | tail]) do
+    case find_duplicate(char, tail) do
+      false -> found_duplicate?(tail)
+      _ -> true
+    end
+  end
+
+  def find_duplicate(_, []), do: false
+  def find_duplicate(char, [head | _   ]) when char == head, do: true
+  def find_duplicate(char, [_    | tail]), do: find_duplicate(char, tail)
+
+end
+
+IsStringUnique.start("")
+IsStringUnique.start("abc")
+IsStringUnique.start("abca")
+IsStringUnique.start("abcaqwfp;yuasrtoiewqf;pyuqlwf;yupasroitenaqwf;pyursaqfwpsrta")
