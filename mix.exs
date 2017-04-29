702e8677f5fc5b73583c7b182255cefbcc715701
Gustavo Saiani <gustavo@poe.ma>
2017-04-29 10:28:20 -0300

    Change required Elixir Version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gs.Mixfile do
   def project do
     [app: :gs,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.2.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
