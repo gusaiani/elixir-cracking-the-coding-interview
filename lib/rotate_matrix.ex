86edf8520c65ae1afbe733bea974e70d37419240
Gustavo Saiani <gustavo@poe.ma>
2017-05-04 11:23:03 -0300

    Add (still) failing test for rotate_matrix

---
+++ lib/rotate_matrix.ex
@@ -0,0 +1,21 @@
+defmodule RotateMatrix do
+  @moduledoc """
+  Chapter 1
+  Page 91
+
+  1.7 Given an image represented by an NxN matrix,
+  where each pixel in the image is 4 bytes,
+  write a method to rotate the image by 90 degrees.
+  Can you do this in place?
+  """
+
+  @doc """
+  Rotate any square matrix 90° clockwise.
+
+  ## Examples
+
+      iex> RotateMatrix.start([0,0])
+      [1,0]
+  """
+  def start(_), do: [3,9]
+end
