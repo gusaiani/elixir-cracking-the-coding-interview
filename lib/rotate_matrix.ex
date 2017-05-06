7596c7c64fa7948a1e77c1f8c5a46ad5701e157d
Gustavo Saiani <gustavo@poe.ma>
2017-05-06 09:47:42 -0300

    Rotate matrix

--- lib/rotate_matrix.ex
+++ lib/rotate_matrix.ex
@@ -17,24 +17,28 @@ defmodule RotateMatrix do
       iex> RotateMatrix.start(['a'])
       ['a']
 
-      iex> four_tile_square = [
-      ...>  'a', 'b', 'c', 'd',
-      ...>  'e', 'f', 'g', 'h',
-      ...>  'i', 'j', 'k', 'l',
-      ...>  'm', 'n', 'o', 'p'
-      ...> ]
-      ...> rotated_four_tile_square = [
-      ...>  'm', 'i', 'e', 'a',
-      ...>  'n', 'j', 'f', 'b',
-      ...>  'o', 'k', 'g', 'c',
-      ...>  'p', 'l', 'h', 'd'
-      ...> ]
-      ...> RotateMatrix.start(four_tile_square) == rotated_four_tile_square
-      true
-
+      iex> RotateMatrix.start(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'])
+      ['m', 'i', 'e', 'a', 'n', 'j', 'f', 'b', 'o', 'k', 'g', 'c', 'p', 'l', 'h', 'd']
   """
   def start(list) when is_list(list) do
+    list_length = length(list)
     row_length = integer_sqrt(list)
+    blank_list = Enum.map(1..list_length, fn _ -> nil end)
+
+    {_, rotated_list} =
+      Enum.reduce(list, {0, blank_list}, fn(entry, {index, new_list}) ->
+        x = rem(index, row_length)
+        y = div(index, row_length)
+
+        new_x = row_length - y - 1
+        new_y = x
+
+        new_position = (new_y * row_length) + new_x
+
+        {index + 1, List.replace_at(new_list, new_position, entry)}
+      end)
+
+    rotated_list
   end
 
   def integer_sqrt(list), do: list |> length |> :math.sqrt |> trunc
